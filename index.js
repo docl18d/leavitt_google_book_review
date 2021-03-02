42857cab9d644a40e99377e13da2e4732db40a1f
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-01 22:26:44 -0700

    heroku push

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
-// Define API routes here
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+};
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/index.html"));
-});
+
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+    console.log(`Server now on port ${PORT}`)
+})
